5d5bf807b58b9f334ae29ae6bc397218e0f54dd5
Waseem Khan <wasim5388@gmail.com>
2022-11-23 22:56:40 +0500

    Added the application to the github - First Commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,48 @@
+import React from "react";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+import ls from "localstorage-slim";
+// enable global encryption
+ls.config.encrypt = true;
+
+function Header(props) {
+  const handleClick = (e) => {
+    // save data in localStorage
+    ls.set("hey", "Hello World");
+    ls.set("userAccessObject", {
+      name: "test1",
+      age: 21,
+    });
+    // data saved gets obfuscated automatically
+    console.log(localStorage.getItem("hey")); // °··ºk¢º½·¯
+    console.log(localStorage.getItem("userAccessObject")); // °··ºk¢º½·¯
+
+    // calling get will deobfuscate data automatically
+    const value = ls.get("hey"); // Hello world
+    console.log("value: ", value);
+
+    const userObj = ls.get("userAccessObject"); // Hello world
+    console.log("userObj: ", userObj);
+  };
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        {/* <HeaderCartButton /> */}
+        <button
+          style={{ width: "150px", height: "50px" }}
+          title="Store data"
+          onClick={handleClick}
+        >
+          Store data
+        </button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A tabe full of delicious food" />
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CartIcon from "./CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+function HeaderCartButton(props) {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+}
+
+export default HeaderCartButton;
